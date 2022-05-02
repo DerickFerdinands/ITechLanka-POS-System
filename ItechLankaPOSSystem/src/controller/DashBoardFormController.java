0e784c38fd9b8159cc66edcc922943cbe6342c66
Hasitha-Lakshan404 <hlakshan614@gmail.com>
2022-05-02 21:19:48 +0530

    Change Dashboard Ui/BarChart

--- ItechLankaPOSSystem/src/controller/DashBoardFormController.java
+++ ItechLankaPOSSystem/src/controller/DashBoardFormController.java
@@ -7,6 +7,9 @@ import com.jfoenix.controls.JFXTextField;
 import db.DBConnection;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -45,6 +48,17 @@ public class DashBoardFormController {
     public ImageView imgSettings;
     public AnchorPane NavigationContext;
 
+    public ImageView imgPules1;
+    public ImageView imgPules2;
+    public ImageView imgPules3;
+    public ImageView imgPules4;
+
+    public Label lblDash1;
+    public Label lblDash2;
+    public Label lblDash3;
+    public Label lblDash4;
+    public BarChart barChartPerformance;
+
     ArrayList<DashboardButton> btnList = new ArrayList<>();
     DashboardButton lastClicked;
     ImageView image = new ImageView(new Image("/Assets/image 222(40).png"));
@@ -55,19 +69,21 @@ public class DashBoardFormController {
         new FadeIn(MainContext).setSpeed(10).play();
         new FadeIn(NavigationContext).setSpeed(5).play();
 
-        btnList.add(new DashboardButton(btnDashboard, imgDash, new Image("/Assets/image (25).png"),95, new Image("/Assets/image (4).png")));
-        btnList.add(new DashboardButton(btnCustomer, imgCus, new Image("/Assets/image (26).png"),172, new Image("/Assets/image (6).png")));
-        btnList.add(new DashboardButton(btnQuote, imgQuote, new Image("/Assets/image (27).png"),243, new Image("/Assets/image (7).png")));
-        btnList.add(new DashboardButton(btnOrders, imgOrder, new Image("/Assets/image (28).png"),316, new Image("/Assets/image (10).png")));
-        btnList.add(new DashboardButton(btnPackages, imgPackage, new Image("/Assets/image (29).png"),389, new Image("/Assets/image (11).png")));
-        btnList.add(new DashboardButton(btnItem, imgItem, new Image("/Assets/image (30).png"),468, new Image("/Assets/image (14).png")));
-        btnList.add(new DashboardButton(btnDIrector, imgDirector, new Image("/Assets/image (31).png"),540, new Image("/Assets/image (15).png")));
-        btnList.add(new DashboardButton(btnSupplier, imgSupply, new Image("/Assets/image (32).png"),611, new Image("/Assets/image (18).png")));
-        btnList.add(new DashboardButton(btnReport, imgReport, new Image("/Assets/image (33).png"),686, new Image("/Assets/image (19).png")));
-        btnList.add(new DashboardButton(btnProfit, imgProfit, new Image("/Assets/image (34).png"),756, new Image("/Assets/image (22).png")));
-        btnList.add(new DashboardButton(btnSettings, imgSettings, new Image("/Assets/image (35).png"),828, new Image("/Assets/image (23).png")));
+        btnList.add(new DashboardButton(btnDashboard, imgDash, new Image("/Assets/image (25).png"), 95, new Image("/Assets/image (4).png")));
+        btnList.add(new DashboardButton(btnCustomer, imgCus, new Image("/Assets/image (26).png"), 172, new Image("/Assets/image (6).png")));
+        btnList.add(new DashboardButton(btnQuote, imgQuote, new Image("/Assets/image (27).png"), 243, new Image("/Assets/image (7).png")));
+        btnList.add(new DashboardButton(btnOrders, imgOrder, new Image("/Assets/image (28).png"), 316, new Image("/Assets/image (10).png")));
+        btnList.add(new DashboardButton(btnPackages, imgPackage, new Image("/Assets/image (29).png"), 389, new Image("/Assets/image (11).png")));
+        btnList.add(new DashboardButton(btnItem, imgItem, new Image("/Assets/image (30).png"), 468, new Image("/Assets/image (14).png")));
+        btnList.add(new DashboardButton(btnDIrector, imgDirector, new Image("/Assets/image (31).png"), 540, new Image("/Assets/image (15).png")));
+        btnList.add(new DashboardButton(btnSupplier, imgSupply, new Image("/Assets/image (32).png"), 611, new Image("/Assets/image (18).png")));
+        btnList.add(new DashboardButton(btnReport, imgReport, new Image("/Assets/image (33).png"), 686, new Image("/Assets/image (19).png")));
+        btnList.add(new DashboardButton(btnProfit, imgProfit, new Image("/Assets/image (34).png"), 756, new Image("/Assets/image (22).png")));
+        btnList.add(new DashboardButton(btnSettings, imgSettings, new Image("/Assets/image (35).png"), 828, new Image("/Assets/image (23).png")));
         lastClicked = btnList.get(0);
 
+        barChartPerformance.lookup(".chart-plot-background").setStyle("-fx-background-color: transparent;");
+        setBarchart();
     }
 
     public void goToItemsOnAction(ActionEvent actionEvent) throws IOException {
@@ -141,7 +157,7 @@ public class DashBoardFormController {
                     button.getBtn().setTextFill(Color.color(0.20, 0.18, 0.29));
                     button.getView().setFitHeight(35);
                     button.getView().setFitWidth(35);
-                    button.getView().setLayoutY(button.getImageLayoutY()-5);
+                    button.getView().setLayoutY(button.getImageLayoutY() - 5);
 
                 } else {
                     button.getBtn().setStyle("-fx-font-size: 13;");
@@ -179,7 +195,6 @@ public class DashBoardFormController {
     }
 
     public void ShowPaneUI(MouseEvent mouseEvent) {
-
         /*image.setFitWidth(353);
         image.setFitHeight(213);
         image.setId("AddedImg");
@@ -214,6 +229,93 @@ public class DashBoardFormController {
     }
 
     public void imagesPulseOnAction(MouseEvent mouseEvent) {
-        new Pulse(image).play();
+
+
+        Object o = mouseEvent.getSource();
+
+        if (o instanceof ImageView) {
+            ImageView i = (ImageView) o;
+
+
+            if(i.getId().equals("imgS")){
+                new Pulse(imgPules1).play();
+                new Pulse(lblDash1).play();
+
+            }else if(i.getId().equals("imgS1")){
+                new Pulse(imgPules2).play();
+                new Pulse(lblDash2).play();
+
+            }else if(i.getId().equals("imgS2")){
+                new Pulse(imgPules3).play();
+                new Pulse(lblDash3).play();
+
+            }else if(i.getId().equals("imgS3")){
+                new Pulse(imgPules4).play();
+                new Pulse(lblDash4).play();
+
+            }
+
+        }
+
+    }
+
+    public void lblMouseEnteredOnAction(MouseEvent mouseEvent) {
+
+        Object o=mouseEvent.getSource();
+
+        if(o instanceof Label){
+            Label i= (Label) o;
+            if(i.getId().equals("lbl1")){
+                imgPules1.setStyle("-fx-effect: dropshadow(gaussian,#6495ED,20,0.0,0,0);");
+            }else if(i.getId().equals("lbl2")){
+                imgPules2.setStyle("-fx-effect: dropshadow(gaussian,#6495ED,20,0.0,0,0);");
+
+            }else if(i.getId().equals("lbl3")){
+                imgPules3.setStyle("-fx-effect: dropshadow(gaussian,#6495ED,20,0.0,0,0);");
+
+            }else if(i.getId().equals("lbl4")){
+                imgPules4.setStyle("-fx-effect: dropshadow(gaussian,#6495ED,20,0.0,0,0);");
+
+            }
+        }
+
+
+    }
+
+    public void lblMouseExitedOnAction(MouseEvent mouseEvent) {
+        Object o=mouseEvent.getSource();
+
+        if(o instanceof Label){
+            Label i= (Label) o;
+            if(i.getId().equals("lbl1")){
+                imgPules1.setStyle(null);
+            }else if(i.getId().equals("lbl2")){
+                imgPules2.setStyle(null);
+            }else if(i.getId().equals("lbl3")){
+                imgPules3.setStyle(null);
+            }else if(i.getId().equals("lbl4")){
+                imgPules4.setStyle(null);
+            }
+        }
+    }
+
+    private void setBarchart(){
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Sample Name");
+        //populating the series with data
+        series.getData().add(new XYChart.Data("January", 577));
+        series.getData().add(new XYChart.Data("February", 22));
+        series.getData().add(new XYChart.Data("March", 452));
+        series.getData().add(new XYChart.Data("April", 245));
+        series.getData().add(new XYChart.Data("May", 87));
+        series.getData().add(new XYChart.Data("June", 447));
+        series.getData().add(new XYChart.Data("July", 699));
+        series.getData().add(new XYChart.Data("August", 58));
+        series.getData().add(new XYChart.Data("September", 45));
+        series.getData().add(new XYChart.Data("October", 252));
+        series.getData().add(new XYChart.Data("November", 45));
+        series.getData().add(new XYChart.Data("December", 25));
+
+        barChartPerformance.getData().add(series);
     }
 }
